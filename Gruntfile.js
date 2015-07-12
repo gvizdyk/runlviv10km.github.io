c41d0b60aecb560a9e62f8f893713cbd4e77c3fb
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2015-07-12 18:35:59 +0300

    added config file for compile less

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            // development: {
+            //     options: {
+            //         paths: ["css"]
+            //     },
+            //     files: {"css/lvivtenrunning.css": "less/bootstrap.less"}
+            // },
+            production: {
+                options: {
+                    paths: ["css"],
+                    compress: true,
+                    optimization: 2,
+                    cleancss: true
+                },
+                files: {"css/lvivtenrunning.css": "less/bootstrap.less"}
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['less/*.less'],
+                tasks: ['less']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.registerTask('default', ['watch']);
+};

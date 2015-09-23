f2e87ee8242a556a22dbee5050fac026fa870765
Yuriy Hvozdovych <gvizdyk@gmail.com>
2015-09-23 18:07:00 +0300

    updated page results-2015

---
+++ js/app.js
@@ -0,0 +1,12 @@
+angular.module('sortApp', [])
+
+.controller('mainController', function($scope, $http) {
+  $scope.sortType     = 'year'; // set the default sort type
+  $scope.sortReverse  = false;  // set the default sort order
+  $scope.searchName   = '';     // set the default search/filter term
+  
+  $http.get('json/results.json').then(function(res){
+    $scope.sushi = res.data;
+  });
+
+});
